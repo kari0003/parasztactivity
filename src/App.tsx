9fc0dab80e7027172f387f116dd667d42d507027
kari0003 <karesz@codingsans.com>
2021-01-24 17:04:15 +0100

    chore(*): setup typescript react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Parasztactivity</h1>
+        <img src={`${process.env.PUBLIC_URL}/fedora.png`} className="App-logo" alt="logo" />
+        <p>Coming Soon</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
