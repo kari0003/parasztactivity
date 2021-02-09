f272ff532a9f43ad73e6891d4ae267fdb0afce85
kari0003 <karesz@codingsans.com>
2021-02-09 22:03:45 +0100

    public socket server

--- src/state/socket.ts
+++ src/state/socket.ts
@@ -3,8 +3,8 @@ import { useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 import { socketHandlerFactory } from './app.service';
 
-const SOCKET_SERVER = 'http://localhost:3005';
-//const SOCKET_SERVER = 'https://parasztactivity.herokuapp.com';
+//const SOCKET_SERVER = 'http://localhost:3005';
+const SOCKET_SERVER = 'https://parasztactivity.herokuapp.com';
 
 export const useSocketHandler = (dispatch: React.Dispatch<AnyAction>) => {
   const { current: socket } = useRef(io.connect(SOCKET_SERVER));
